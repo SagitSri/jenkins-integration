5479282f4bade96207ab4fc368c9fe4051a9fbbc
SagitSri <sagit8529@gmail.com>
2023-02-21 10:43:22 +0530

    Initial Commit on SRC

---
+++ src/main/java/com/twilight/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.twilight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+    
+    @GetMapping("/test")
+    public String jenkinsTest(){
+        return "CI/CD by Jenkins with Docker";
+    }
+
+}
